2c76179dc4f9a60f443123af6c26f4753fe3d64a
oluigifreire <luigiborgheti@gmail.com>
2026-03-20 11:31:37 -0300

    Aula 2 variáveis e operadores

---
+++ Desafios Lição.py
@@ -0,0 +1,14 @@
+# DESAFIO 1
+
+nome = input("Me diga seu nome: ")
+
+print(f"Boas vindas ao pycharm {nome}!")
+
+#DESAFIO 2
+
+print("Preencha os dados utilizando apenas números.")
+nascimento_dia = int(input("Em que dia você nasceu? "))
+nascimento_mes = int(input("De qual mês? "))
+nascimento_ano = int(input("Em qual ano? "))
+
+print(f"{nome}, você nasceu em {nascimento_dia}/{nascimento_mes}/{nascimento_ano}")
---
+++ aula02 variaveis operadores/aula02.py
@@ -0,0 +1,29 @@
+print("Hello World")
+
+print(6+5)
+print("5 + 6")
+print("5" + "6") # Concatenação de strings
+
+# Comentário de 1 linha
+'''
+Comentários 
+de 
+múltiplas linhas
+'''
+
+# Variáveis
+nome = "Papa" # str
+idade = 18 # int
+peso = 83.5 # float
+print(nome, idade, peso)
+print(f"Eaii {nome}!")
+
+# INPUT - SIMULAÇÃO DE UM FORMS NO CMD
+nome = input('digite seu nome: ')
+idade = int(input('digite sua idade: '))
+print(nome, idade)
+nova_idade = idade + 1
+# em nome de variáveis, não se começa com numeros, nem caracteres especiais e nem acentos nem maiusculas se usa
+
+print(f"Parabéns! você fez {nova_idade} anos!")
+
